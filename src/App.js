3a9aa14a2a3a9a751fe8b2e73ca54ed834498aac
unknown <jamalnasirone52@gmail.com>
2024-05-18 20:33:58 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import Appp from "./components/website";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Accordian /> */}
+      {/* <RandomColor /> */}
+      {/* <StarRating noOfStars={10}/> */}
+      {/* <Appp /> */}
+      {/* <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        limit={10}
+        page={1}
+      /> */}
+      <LoadMoreData />
     </div>
   );
 }
